8aed8dc4028ea8e08de8091aed97c59042831a21
Peter Hutchison <Peterhutch03@gmail.com>
2022-03-01 10:17:44 -0600

    Initial Commit

---
+++ Za Hunter Final/Za_Hunter_FinalApp.swift
@@ -0,0 +1,17 @@
+//
+//  Za_Hunter_FinalApp.swift
+//  Za Hunter Final
+//
+//  Created by Student on 3/1/22.
+//
+
+import SwiftUI
+
+@main
+struct Za_Hunter_FinalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
